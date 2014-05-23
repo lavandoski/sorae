f9297d25ccfcc09a9a7330818689c710bf77caf7
lavandoski <lavandoski@gmail.com>
2014-05-23 20:02:23 -0300

    lab

---
+++ protected/tests/fixtures/User.php
@@ -0,0 +1,18 @@
+<?php
+
+return array(
+	/*
+	'sample1'=>array(
+		'professor_id' => '',
+		'username' => '',
+		'password' => '',
+		'email' => '',
+	),
+	'sample2'=>array(
+		'professor_id' => '',
+		'username' => '',
+		'password' => '',
+		'email' => '',
+	),
+	*/
+);
